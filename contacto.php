67347ac781de335050e24f969e7335726debee3b
MartaCab96 <martacabrera96@gmail.com>
2020-05-15 12:33:36 +0100

    Add files via upload

---
+++ contacto.php
@@ -0,0 +1,10 @@
+<?php
+$nombre =$_POST['nombre']
+$apellido =$_POST['apellido']
+$telefono =$_POST['telefono']
+$email =$_POST['email']
+$cp =$_POST['cp']
+$texto =$_POST['texto']
+
+echo $nombre
+?>
